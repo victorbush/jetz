b4384688bb16ebf68fbaf1f860d9a49792747112
Victor Bushong <victorbush@gmail.com>
2020-09-26 13:55:33 -0500

    Save work

--- Source/jetz/gpu/vlk/vlk_model.cpp
+++ Source/jetz/gpu/vlk/vlk_model.cpp
@@ -9,6 +9,8 @@ INCLUDES
 #include <memory>
 
 #include "jetz/gpu/vlk/vlk_model.h"
+#include "jetz/gpu/vlk/vlk_texture.h"
+#include "jetz/gpu/vlk/pipelines/vlk_pipeline_cache.h"
 #include "jetz/main/log.h"
 
 /*=============================================================================
@@ -24,11 +26,13 @@ CONSTRUCTORS
 vlk_model::vlk_model
 	(
 	vlk_device&					dev,
-	uptr<tinygltf::Model>		gltf
+	uptr<tinygltf::Model>		gltf,
+	vlk_pipeline_cache&			pipeline_cache
 	)
 	: 
 	_device(dev),
-	_gltf(std::move(gltf))
+	_gltf(std::move(gltf)),
+	_pipeline_cache(pipeline_cache)
 {
 	create_buffers();
 	create_vertex_input_bindings();
@@ -48,6 +52,10 @@ vlk_model::~vlk_model()
 
 /*=============================================================================
 PUBLIC METHODS
+=============================================================================*/
+
+/*=============================================================================
+PRIVATE METHODS
 =============================================================================*/
 
 void vlk_model::create_buffers()
@@ -78,6 +86,158 @@ void vlk_model::create_buffers()
 	}
 }
 
+void vlk_model::create_materials()
+{
+	for (const auto& mat : _gltf->materials)
+	{
+		loadMaterial(mat);
+	}
+}
+
+void vlk_model::create_pipelines()
+{
+	/*
+	Create vertex input attribute descriptions.
+	Find pipeline for each mesh primitive.
+	*/
+	size_t numPrimitives = 0;
+	_primitives.resize(_gltf->meshes.size());
+
+	for (size_t i = 0; i < _gltf->meshes.size(); ++i)
+	{
+		const auto& mesh = _gltf->meshes[i];
+
+		for (size_t j = 0; j < mesh.primitives.size(); ++j)
+		{
+			const auto& prim = mesh.primitives[j];
+			std::vector<VkVertexInputAttributeDescription> attributeDescriptions;
+
+			// Function to load a vertex attribute description
+			auto loadAttribute = [this, prim, &attributeDescriptions](attribute_type type)
+			{
+				// Try to find the attribute in this primitve
+				auto attr = prim.attributes.find(attribute_names[type]);
+				if (attr != prim.attributes.end())
+				{
+					assert(attribute_locations[type] >= 0);
+
+					const auto& accessor = _gltf->accessors[attr->second];
+
+					VkVertexInputAttributeDescription desc = {};
+					desc.binding = accessor.bufferView;
+					desc.location = attribute_locations[type];
+					desc.format = get_vk_format(accessor.type, accessor.componentType);
+					desc.offset = static_cast<uint32_t>(accessor.byteOffset);
+
+					attributeDescriptions.push_back(desc);
+				}
+			};
+
+			loadAttribute(POSITION);
+			loadAttribute(NORMAL);
+			//loadAttribute(TANGENT);
+			loadAttribute(TEXCOORD_0);
+			//loadAttribute(TEXCOORD_1);
+			//loadAttribute(COLOR_0);
+			//loadAttribute(JOINTS_0);
+			//loadAttribute(WEIGHTS_0;
+
+			/* Get a pipeline for this mesh primitive */
+			auto create_info = vlk_pipeline_create_info();
+			create_info.vertex_attribute_descriptions = attributeDescriptions;
+			create_info.vertex_binding_descriptions = _vertex_binding_descriptions;
+			const auto& pipeline = _pipeline_cache.create_gltf_pipeline(create_info);
+
+			/* Create a primitive wrapper to store data for this primitive */
+			auto p = Primitive(mesh.primitives[j], pipeline);
+			p.PrimID = numPrimitives++;
+			p.MeshIndex = i;
+			p.PrimIndex = j;
+			p.Material = getVulkanMaterial(prim.material);
+			_primitives[i].push_back(p);
+		}
+	}
+}
+
+void vlk_model::create_textures()
+{
+	for (const auto& img : _gltf.images)
+	{
+		loadTexture(img);
+	}
+}
+
+/**
+Creates vertex input bindings
+*/
+void vlk_model::create_vertex_input_bindings()
+{
+	std::unordered_map<int, size_t> bindingCache;
+	size_t bindingIndex = 0;
+
+	/*
+	We only need bindings for buffer views that are used for indices and vertex input
+	attributes (position, normal, etc). Buffer view usage is defined by accessor,
+	so go through accessors and create bindings for each accessor, if appropriate.
+
+	If a buffer view is only used by one accessor, then that accesor will determine
+	the byte stride to use.
+
+	If a buffer view is used by two or more accessors, the buffer view must define
+	the byte stride.
+	*/
+	for (size_t i = 0; i < _gltf.accessors.size(); ++i)
+	{
+		const auto& a = _gltf.accessors[i];
+		const auto& bv = _gltf.bufferViews[a.bufferView];
+
+		/* Check if binding description already created for this buffer view */
+		auto bi = bindingCache.find(a.bufferView);
+		if (bi != bindingCache.end())
+		{
+			if (bv.byteStride == 0)
+			{
+				// TODO : non fatal error
+				throw std::runtime_error("Buffer views that are accessed by more than one accessor should define a byte stride.");
+			}
+
+			/* Already processed this buffer view */
+			continue;
+		}
+
+		/* Determine byte stride */
+		int byteStride = a.ByteStride(bv);
+
+		if (byteStride < 0)
+		{
+			// TODO : make non fatal and log error
+			throw std::runtime_error("Invalid byte stride.");
+
+			byteStride = 0;
+		}
+
+		/* Create new binding */
+		_vertexBindingBuffers.resize(bindingIndex + 1);
+		_vertexBindingOffsets.resize(bindingIndex + 1);
+		_vertexBindingDescriptions.resize(bindingIndex + 1);
+
+		/* Map the buffer view's buffer index to a GPU buffer */
+		_vertexBindingBuffers[bindingIndex] = _buffers[bv.buffer]->GetHandle();
+
+		/* Record byte offset into buffer */
+		_vertexBindingOffsets[bindingIndex] = static_cast<VkDeviceSize>(bv.byteOffset);
+
+		/* Create the vertex binding description */
+		_vertexBindingDescriptions[bindingIndex] = {};
+		_vertexBindingDescriptions[bindingIndex].binding = static_cast<uint32_t>(bindingIndex);
+		_vertexBindingDescriptions[bindingIndex].stride = static_cast<uint32_t>(byteStride);
+		_vertexBindingDescriptions[bindingIndex].inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+		/* Add to the temporary cache so we know this buffer view has been processed */
+		bindingCache[a.bufferView] = bindingIndex++;
+	}
+}
+
 void vlk_model::destroy_buffers()
 {
 	for (size_t i = 0; i < _buffers.size(); ++i)
@@ -86,8 +246,426 @@ void vlk_model::destroy_buffers()
 	}
 }
 
-/*=============================================================================
-PRIVATE METHODS
-=============================================================================*/
+void vlk_model::destroy_materials()
+{
+	for (auto& mat : _materials)
+	{
+		mat.reset();
+	}
+
+	_materials.clear();
+}
+
+void vlk_model::destroy_pipelines()
+{
+	/* Let the pipeline cache handle cleanup */
+}
+
+void vlk_model::destroy_textures()
+{
+	for (auto& tex : _textures)
+	{
+		tex.reset();
+	}
+
+	_textures.clear();
+}
+
+void vlk_model::destroy_vertex_input_bindings()
+{
+	/* Nothing to do */
+}
+
+/**
+Gets a Vulkan format from a GLTF type and format.
+
+gltfType - TINYGLTF_TYPE_*
+gltfComponentType - TINYGLTF_COMPONENT_TYPE_*
+*/
+VkFormat vlk_model::get_vk_format(int gltfType, int gltfComponentType) const
+{
+	if (gltfType == TINYGLTF_TYPE_VEC2)
+	{
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_BYTE)
+			return VK_FORMAT_R8G8_SINT;
+		//if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_DOUBLE)
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_FLOAT)
+			return VK_FORMAT_R32G32_SFLOAT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_INT)
+			return VK_FORMAT_R32G32_SINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_SHORT)
+			return VK_FORMAT_R16G16_SINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE)
+			return VK_FORMAT_R8G8_UINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT)
+			return VK_FORMAT_R32G32_UINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT)
+			return VK_FORMAT_R16G16_UINT;
+	}
+	else if (gltfType == TINYGLTF_TYPE_VEC3)
+	{
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_BYTE)
+			return VK_FORMAT_R8G8B8_SINT;
+		//if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_DOUBLE)
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_FLOAT)
+			return VK_FORMAT_R32G32B32_SFLOAT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_INT)
+			return VK_FORMAT_R32G32B32_SINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_SHORT)
+			return VK_FORMAT_R16G16B16_SINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE)
+			return VK_FORMAT_R8G8B8_UINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT)
+			return VK_FORMAT_R32G32B32_UINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT)
+			return VK_FORMAT_R16G16B16_UINT;
+	}
+	else if (gltfType == TINYGLTF_TYPE_VEC4)
+	{
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_BYTE)
+			return VK_FORMAT_R8G8B8A8_SINT;
+		//if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_DOUBLE)
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_FLOAT)
+			return VK_FORMAT_R32G32B32A32_SFLOAT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_INT)
+			return VK_FORMAT_R32G32B32A32_SINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_SHORT)
+			return VK_FORMAT_R16G16B16A16_SINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE)
+			return VK_FORMAT_R8G8B8A8_UINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT)
+			return VK_FORMAT_R32G32B32A32_UINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT)
+			return VK_FORMAT_R16G16B16A16_UINT;
+	}
+	else if (gltfType == TINYGLTF_TYPE_SCALAR)
+	{
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_BYTE)
+			return VK_FORMAT_R8_SINT;
+		//if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_DOUBLE)
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_FLOAT)
+			return VK_FORMAT_R32_SFLOAT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_INT)
+			return VK_FORMAT_R32_SINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_SHORT)
+			return VK_FORMAT_R16_SINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE)
+			return VK_FORMAT_R8_UINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT)
+			return VK_FORMAT_R32_UINT;
+		if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT)
+			return VK_FORMAT_R16_UINT;
+	}
+	//else if (gltfType == TINYGLTF_TYPE_MAT2)
+	//else if (gltfType == TINYGLTF_TYPE_MAT3)
+	//else if (gltfType == TINYGLTF_TYPE_MAT4)
+	//else if (gltfType == TINYGLTF_TYPE_MATRIX)
+	//else if (gltfType == TINYGLTF_TYPE_VECTOR)
+
+	LOG_FATAL("Unknown format");
+}
+
+/**
+Gets a Vulkan format from a GLTF type and format.
+
+gltfComponentType - TINYGLTF_COMPONENT_TYPE_*
+*/
+VkIndexType vlk_model::get_vk_index_type(int gltfComponentType) const
+{
+	if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT)
+		return VK_INDEX_TYPE_UINT32;
+	if (gltfComponentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT)
+		return VK_INDEX_TYPE_UINT16;
+
+	throw new std::runtime_error("Unknown format");
+}
+
+wptr<vlk_material> vlk_model::get_vulkan_material(int index)
+{
+	if (index < 0 || index >= _materials.size())
+	{
+		throw std::runtime_error("Invalid material index.");
+	}
+
+	return _materials[index];
+}
+
+wptr<vlk_texture> vlk_model::get_vulkan_texture(int index)
+{
+	if (index < 0 || index >= _gltf->textures.size())
+	{
+		LOG_ERROR("Invalid texture index.");
+		return wptr<vlk_texture>();
+	}
+
+	auto imgIdx = _gltf.textures[index].source;
+
+	if (imgIdx < 0 || imgIdx >= _textures.size())
+	{
+		LOG_ERROR("Invalid image index.");
+		return wptr<vlk_texture>();
+	}
+
+	return _textures[imgIdx];
+}
+
+void vlk_model::load_material(const tinygltf::Material& mat)
+{
+	auto material = new VulkanMaterial(_device);
+
+	/*
+	Normal/occlusion/emissive
+	*/
+
+	/* Alpha mode/cutoff */
+	// TODO
+
+	/* Double/sided */
+	// TODO
+
+	/* Normal texture */
+	auto addAttr = mat.additionalValues.find(MAT_NORMAL_TEXTURE);
+	if (addAttr != mat.additionalValues.end())
+	{
+		auto texIdx = addAttr->second.TextureIndex();
+		material->NormalTexture = getVulkanTexture(texIdx);
+	}
+
+	/* Emissive factor */
+	addAttr = mat.additionalValues.find(MAT_EMISSIVE_FACTOR);
+	if (addAttr != mat.additionalValues.end())
+	{
+		material->EmissiveFactor = glm::make_vec3(addAttr->second.number_array.data());
+	}
+
+	/* Emissive texture */
+	addAttr = mat.additionalValues.find(MAT_EMISSIVE_TEXTURE);
+	if (addAttr != mat.additionalValues.end())
+	{
+		auto texIdx = addAttr->second.TextureIndex();
+		material->EmissiveTexture = getVulkanTexture(texIdx);
+	}
+
+	/* Occlusion texture */
+	// TODO
+
+	/*
+	PBR Metallic/Roughness
+	*/
+
+	/* Base color factor */
+	auto attr = mat.values.find(MAT_BASE_COLOR_FACTOR);
+	if (attr != mat.values.end())
+	{
+		material->BaseColorFactor = glm::vec4(glm::make_vec4(attr->second.ColorFactor().data()));
+	}
+
+	/* Base color texture */
+	attr = mat.values.find(MAT_BASE_COLOR_TEXTURE);
+	if (attr != mat.values.end())
+	{
+		auto texIdx = attr->second.TextureIndex();
+		material->BaseColorTexture = getVulkanTexture(texIdx);
+	}
+
+	/* Metallic factor */
+	attr = mat.values.find(MAT_METALLIC_FACTOR);
+	if (attr != mat.values.end())
+	{
+		material->MetallicFactor = static_cast<float>(attr->second.Factor());
+	}
+
+	/* Roughness factor */
+	attr = mat.values.find(MAT_ROUGHNESS_FACTOR);
+	if (attr != mat.values.end())
+	{
+		material->RoughnessFactor = static_cast<float>(attr->second.Factor());
+	}
+
+	/* Metallic/roughness texture */
+	attr = mat.values.find(MAT_METALLIC_ROUGHNESS_TEXTURE);
+	if (attr != mat.values.end())
+	{
+		auto texIdx = attr->second.TextureIndex();
+		material->MetallicRoughnessTexture = getVulkanTexture(texIdx);
+	}
+
+	/*
+	Save material
+	*/
+	_materials.push_back(std::shared_ptr<VulkanMaterial>(material));
+}
+
+void vlk_model::load_texture(const tinygltf::Image& image)
+{
+	TextureCreateInfo createInfo = {};
+	createInfo.Data = (void*)image.image.data();
+	createInfo.Size = image.image.size();
+	createInfo.Width = static_cast<uint32_t>(image.width);
+	createInfo.Height = static_cast<uint32_t>(image.height);
+	createInfo.Type = TEXTURE_TYPE_2D;
+
+	/* Create and store texture */
+	auto texture = new VulkanTexture(_device, createInfo);
+	_textures.push_back(std::shared_ptr<VulkanTexture>(texture));
+}
+
+void vlk_model::render_mesh
+	(
+	size_t							index,
+	const RenderFrame&				frame,
+	VkCommandBuffer					cmd,
+	glm::mat4						transform,
+	const VulkanModelInstance& instance
+	) const
+{
+	/* Validate index */
+	if (index >= _gltf.meshes.size())
+	{
+		/* Invalid index */
+		return;
+	}
+
+	/* Get the mesh */
+	const auto& mesh = _gltf.meshes[index];
+
+	// Bind vertex buffers for mesh
+	vkCmdBindVertexBuffers(cmd, 0, static_cast<uint32_t>(_vertexBindingBuffers.size()), _vertexBindingBuffers.data(), _vertexBindingOffsets.data());
+
+	/*
+	Render each mesh component (GLTF "primitives")
+	*/
+	for (size_t i = 0; i < mesh.primitives.size(); ++i)
+	{
+		const auto& prim = _primitives[index][i];
+
+		/*
+		Bind pipeline for primitive
+		*/
+		prim.Pipeline.Bind(cmd);
+
+		/*
+		Update instance descriptor set for primitive
+		*/
+		auto descSet = instance.GetDescriptorSet(prim.PrimID);
+		auto d = descSet.lock();
+
+		if (d == nullptr) throw new std::runtime_error("What?");
+
+		PerMeshInstanceUBO data = {};
+		data.modelMatrix = transform;
+
+		auto mat = prim.Material.lock();
+		if (mat != nullptr)
+		{
+			data.BaseColorFactor = mat->BaseColorFactor;
+		}
+
+		d->UpdateUniformBuffer(frame, data);
+
+		/*
+		Bind instance descriptor set for primitive
+		*/
+		d->Bind(frame, cmd, prim.Pipeline.GetLayoutHandle());
+
+		/*
+		Bind index buffer for primitive
+		*/
+		auto a = _gltf.accessors[prim.Data.indices];
+		auto bv = _gltf.bufferViews[a.bufferView];
+		auto offset = a.byteOffset + bv.byteOffset;
+		auto idxType = getVkIndexType(a.componentType);
+
+		vkCmdBindIndexBuffer(cmd, _buffers[bv.buffer]->GetHandle(), offset, idxType);
+
+		/*
+		Draw indexed
+		*/
+		vkCmdDrawIndexed(cmd, static_cast<uint32_t>(a.count), 1, 0, 0, 0);
+	}
+}
+
+void vlk_model::render_node
+	(
+	size_t							index,
+	const RenderFrame& frame,
+	VkCommandBuffer					cmd,
+	glm::mat4						parentTransform,
+	const VulkanModelInstance& instance
+	) const
+{
+	/* Validate index */
+	// TOOD : the GLTF loader should be trusted to provide validate indices
+	if (index >= _gltf.nodes.size())
+	{
+		/* Invalid index */
+		return;
+	}
+
+	/* Get the node */
+	const auto& node = _gltf.nodes[index];
+
+	/*
+	Compute the transform for this node
+	*/
+	glm::mat4 transform = glm::mat4(parentTransform);
+
+	if (node.matrix.size() == 16)
+	{
+		// Node uses a transformation matrix
+		auto matrix = glm::make_mat4x4(node.matrix.data());
+		transform = transform * glm::mat4(matrix);
+	}
+	else
+	{
+		// Node either uses translation, rotation, and scale vectors, or has no transform
+
+		/* Scale */
+		if (node.scale.size() == 3)
+		{
+			// TODO : potentially make tinygltf allow float vectors/matrices so don't have to convert all the time.
+			// either that or convert and store manually.
+			auto scale = glm::vec3(glm::make_vec3(node.scale.data()));
+			transform = glm::scale(transform, scale);
+		}
+
+		/* Rotation */
+		if (node.rotation.size() == 4)
+		{
+			auto rotateQuat = glm::highp_fquat(glm::make_quat(node.rotation.data()));
+			transform = transform * glm::mat4_cast(rotateQuat);
+		}
+
+		/* Translation */
+		if (node.translation.size() == 3)
+		{
+			auto translation = glm::vec3(glm::make_vec3(node.translation.data()));
+			transform = glm::translate(transform, translation);
+		}
+	}
+
+	/*
+	A node can contain a mesh or a camera, or it can be empty and just define a transform
+	*/
+	if (node.mesh >= 0 && node.mesh < _gltf.meshes.size())
+	{
+		/* Node points to mesh */
+		renderMesh(node.mesh, frame, cmd, transform, instance);
+	}
+	else if (node.camera >= 0 && node.camera < _gltf.cameras.size())
+	{
+		/* Node points to camera */
+	}
+	else
+	{
+		/* Transform only */
+	}
+
+	/* Render child nodes */
+	for (auto childIndex : node.children)
+	{
+		renderNode(childIndex, frame, cmd, transform, instance);
+	}
+}
 
 }   /* namespace jetz */
--- Source/jetz/gpu/vlk/vlk_model.h
+++ Source/jetz/gpu/vlk/vlk_model.h
@@ -8,6 +8,7 @@ vlk_model.h
 INCLUDES
 =============================================================================*/
 
+#include <glm/glm.hpp>
 #include <vector>
 #include <vulkan/vulkan.h>
 
@@ -15,6 +16,8 @@ INCLUDES
 #include "jetz/gpu/gpu_model.h"
 #include "jetz/gpu/vlk/vlk_buffer.h"
 #include "jetz/gpu/vlk/vlk_device.h"
+#include "jetz/gpu/vlk/vlk_material.h"
+#include "jetz/gpu/vlk/pipelines/vlk_gltf_pipeline.h"
 #include "thirdparty/tinygltf/tiny_gltf.h"
 
 /*=============================================================================
@@ -27,12 +30,75 @@ namespace jetz {
 TYPES
 =============================================================================*/
 
+class vlk_pipeline_chache
+
 class vlk_model : public gpu_model {
 
 public:
 
-	vlk_model(vlk_device& dev, uptr<tinygltf::Model> gltf);
+	vlk_model(vlk_device& dev, uptr<tinygltf::Model> gltf, vlk_pipeline_cache& pipeline_cache);
 	virtual ~vlk_model() override;
+
+	/*-----------------------------------------------------
+	Public Types
+	-----------------------------------------------------*/
+	enum attribute_type
+	{
+		POSITION,
+		NORMAL,
+		TANGENT,
+		TEXCOORD_0,
+		TEXCOORD_1,
+		COLOR_0,
+		JOINTS_0,
+		WEIGHTS_0
+	};
+
+	/*-----------------------------------------------------
+	Public Variables
+	-----------------------------------------------------*/
+	const std::vector<std::string> attribute_names =
+	{
+		"POSITION",
+		"NORMAL",
+		"TANGENT",
+		"TEXCOORD_0",
+		"TEXCOORD_1",
+		"COLOR_0",
+		"JOINTS_0",
+		"WEIGHTS_0"
+	};
+
+	// Locations hard coded in shader right now.
+	// -1 means not used.
+	const std::vector<int> attribute_locations =
+	{
+		0,		// POSITION
+		1,		// NORMAL
+		-1,		// TANGENT
+		2,		// TEXCOORD_0
+		-1,		// TEXCOORD_1
+		-1,		// COLOR_0
+		-1,		// JOINTS_0
+		-1		// WEIGHTS_0
+	};
+
+	/* Material attributes */
+	const char* MAT_PBR_METALLIC_ROUGHNESS = "pbrMetallicRoughness";
+	const char* MAT_ALPHA_CUTOFF = "alphaCutoff";
+	const char* MAT_ALPHA_MODE = "alphaMode";
+	const char* MAT_DOUBLE_SIDED = "doubleSided";
+	const char* MAT_EMISSIVE_FACTOR = "emissiveFactor";
+	const char* MAT_EMISSIVE_TEXTURE = "emissiveTexture";
+	const char* MAT_NORMAL_TEXTURE = "normalTexture";
+	const char* MAT_OCCLUSION_TEXTURE = "occlusionTexture";
+
+	/* Material - PBR */
+	const char* MAT_BASE_COLOR_FACTOR = "baseColorFactor";
+	const char* MAT_BASE_COLOR_TEXTURE = "baseColorTexture";
+	const char* MAT_METALLIC_FACTOR = "metallicFactor";
+	const char* MAT_ROUGHNESS_FACTOR = "roughnessFactor";
+	const char* MAT_METALLIC_ROUGHNESS_TEXTURE = "metallicRoughnessTexture";
 
 	/*-----------------------------------------------------
 	Public Methods
@@ -45,13 +111,46 @@ protected:
 	-----------------------------------------------------*/
 
 private:
+	/*-----------------------------------------------------
+	Class to encapsulate GLTF mesh primitives
+	-----------------------------------------------------*/
+	class Primitive
+	{
+	public:
+		size_t						id;			/* primitive ID for the GLTF model */
+		size_t						mesh_index;	/* mesh index */
+		size_t						prim_index;	/* primitive index within the mesh */
+
+		const tinygltf::Primitive&	data;		/* the actual primitive data from the GLTF model */
+		const vlk_gltf_pipeline&	pipeline;	/* pipeline used to render this mesh primitive */
+		wptr<vlk_material>			material;
+		VkIndexType					index_type;
+
+		Primitive(const tinygltf::Primitive& data, const vlk_gltf_pipeline& pipeline)
+			: data(data), pipeline(pipeline), id(0), mesh_index(0), prim_index(0), index_type(VK_INDEX_TYPE_UINT16) {}
+	};
 
 	/*-----------------------------------------------------
 	Private methods
 	-----------------------------------------------------*/
 
 	void create_buffers();
+	void create_materials();
+	void create_pipelines();
+	void create_textures();
+	void create_vertex_input_bindings();
 	void destroy_buffers();
+	void destroy_materials();
+	void destroy_pipelines();
+	void destroy_textures();
+	void destroy_vertex_input_bindings();
+
+	VkFormat				get_vk_format(int gltfType, int gltfComponentType) const;
+	VkIndexType				get_vk_index_type(int gltfComponentType) const;
+	wptr<vlk_material>		get_vulkan_material(int index);
+	wptr<vlk_texture>		get_vulkan_texture(int index);
+	void					load_material(const tinygltf::Material& mat);
+	void					load_texture(const tinygltf::Image& image);
 
 	/*-----------------------------------------------------
 	Private variables
@@ -60,13 +159,23 @@ private:
 	/*
 	Dependencies
 	*/
-	vlk_device&					_device;
-	uptr<tinygltf::Model>		_gltf;
+	vlk_device&						_device;
+	uptr<tinygltf::Model>			_gltf;
+	vlk_pipeline_cache&				_pipeline_cache;
 
 	/*
 	Create/destroy
 	*/
 	std::vector<uptr<vlk_buffer>>	_buffers;
+
+	/* A list of primitives for each mesh */
+	std::vector<std::vector<Primitive>>		_primitives;
+
+	/* Vertex input binding data */
+	std::vector<VkBuffer>			_vertex_binding_buffers;
+	std::vector<VkDeviceSize>		_vertex_binding_offsets;
+	std::vector<VkVertexInputBindingDescription>
+									_vertex_binding_descriptions;
 
 	/*
 	Other
